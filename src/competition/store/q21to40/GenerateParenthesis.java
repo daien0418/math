59ba24b0bb0082a3e21a00ba0b2b7bc66f0acd5f
daien <daien@bonc.com.cn>
2018-08-10 17:24:37 +0800

    question22

---
+++ src/competition/store/q21to40/GenerateParenthesis.java
@@ -0,0 +1,11 @@
+package competition.store.q21to40;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParenthesis {
+	public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        return list;
+    }
+}
