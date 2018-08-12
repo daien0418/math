4d6e52cf6799da3f36e04be068fcba0ccee7008a
Administrator <daien1024@sina.com>
2018-08-12 23:42:26 +0800

    2018年8月12日23:42:23

---
+++ src/competition/store/q21to40/CombinationSum.java
@@ -0,0 +1,18 @@
+package competition.store.q21to40;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        
+        return list;
+    }
+	
+	public static void main(String[] args) {
+
+	}
+
+}
