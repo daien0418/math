b64d0b4123c9ff393a3ded5feff6234c64f9b200
daien <daien@bonc.com.cn>
2018-08-20 18:17:47 +0800

    1题目

---
+++ src/competition/store/q81to100/ReverseBetween.java
@@ -0,0 +1,47 @@
+package competition.store.q81to100;
+
+public class ReverseBetween {
+
+	public ListNode reverseBetween(ListNode head, int m, int n) {
+		ListNode vir = new ListNode(0);
+		vir.next = head;
+		ListNode ret = vir;
+
+//		if(m>=n) return ret.next;   //ø…“‘»•µÙ
+
+		int times = 0;
+		while(times<m-1){
+			vir = vir.next;
+			times++;
+		}
+
+		ListNode h = vir;
+		ListNode t = vir.next;
+		ListNode pre = vir.next;
+		ListNode aft = pre.next;
+		ListNode temp = null;
+
+		times = 1;
+		while(times<=n-m){
+			temp = aft.next;
+			aft.next = pre;
+			pre = aft;
+			aft = temp;
+			times++;
+		}
+
+		h.next = pre;
+		t.next = aft;
+
+		return ret.next;
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
