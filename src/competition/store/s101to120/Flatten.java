855d96b9862b563d8d9f61f69ee751059f9eb500
daien <daien@bonc.com.cn>
2018-08-27 17:45:56 +0800

    3个

--- src/competition/store/s101to120/Flatten.java
+++ src/competition/store/s101to120/Flatten.java
@@ -5,7 +5,23 @@ import competition.structure.TreeNode;
 public class Flatten {
 
 	public void flatten(TreeNode root) {
+		TreeNode node = root;
+		while(node!=null){
+			if(node.left!=null){
+				TreeNode right = node.right;
+				TreeNode left = node.left;
+				node.right = node.left;
+				node.left =null;
 
+				if(right!=null){
+					while(left.right!=null){
+						left=left.right;
+					}
+					left.right = right;
+				}
+			}
+			node = node.right;
+		}
 	}
 
 	public static void main(String[] args) {
---
+++ src/competition/store/s121to140/SumNumbers.java
@@ -0,0 +1,53 @@
+package competition.store.s121to140;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+import competition.structure.TreeNode;
+
+public class SumNumbers {
+
+	public int sumNumbers(TreeNode root) {
+		int sum = 0;
+		int subSum = 0;
+
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		Set<TreeNode> set = new HashSet<TreeNode>();
+		if (root != null) {
+			stack.push(root);
+		}
+
+		while (!stack.isEmpty()) {
+			TreeNode node = stack.pop();
+
+			if (node.left == null && node.right == null) {
+				if (!set.contains(node)) {
+					subSum = subSum * 10 + node.val;
+					sum = sum + subSum;
+				}
+
+				subSum = (subSum - node.val) / 10;
+			} else {
+				subSum = subSum * 10 + node.val;
+				stack.push(node);
+				if (node.right != null) {
+					stack.push(node.right);
+				}
+				if (node.left != null) {
+					stack.push(node.left);
+				}
+				set.add(node);
+			}
+			node.right = null;
+			node.left = null;
+		}
+
+		return sum;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
