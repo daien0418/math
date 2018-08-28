b51cde8134543ffaa45b2b1d46a129cc2834655c
daien <daien@bonc.com.cn>
2018-08-28 18:28:16 +0800

    没做

---
+++ src/competition/store/q81to100/NumTrees.java
@@ -0,0 +1,14 @@
+package competition.store.q81to100;
+
+public class NumTrees {
+
+	public int numTrees(int n) {
+		int ret = 0;
+		return ret;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
