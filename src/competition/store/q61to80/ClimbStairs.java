b788bb2c9473cf4392740a8de57051367d60b528
daien <daien@bonc.com.cn>
2018-08-28 18:20:48 +0800

    4个，爬楼梯没做

--- src/competition/store/q61to80/ClimbStairs.java
+++ src/competition/store/q61to80/ClimbStairs.java
@@ -1,21 +1,41 @@
 package competition.store.q61to80;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClimbStairs {
 
 	public int climbStairs(int n) {
-		int ret = 0;
-		int i=0;
-		while(i*2<=n){
-//			ret = ret + c(i,(n-i*2)+i);
-			i=i+1;
+		List<String> list = new ArrayList<String>();
+		list.add(new String(""));
+
+		for (int i = 1; i <= n; i++) {
+			int size = list.size();
+			for (int j = 0; j < size; j++) {
+				String str = list.get(0);
+				for (int k = 0; k <= str.length(); k++) {
+					String newStr = str.substring(0, k) + "1" + str.substring(k, str.length());
+					if (!list.contains(newStr)) {
+						list.add(newStr);
+					}
+				}
+				list.remove(0);
+			}
+			if (i % 2 == 0) {
+				String ful2 = new String("");
+				for (int m = 0; m < i / 2; m++) {
+					ful2 = ful2 + "2";
+				}
+				list.add(ful2);
+			}
 		}
 
-		return ret;
+		return list.size();
 	}
 
 	public static void main(String[] args) {
 		ClimbStairs climbStairs = new ClimbStairs();
-		System.out.println(climbStairs.climbStairs(19));
+		System.out.println(climbStairs.climbStairs(5));
 	}
 
 }
---
+++ src/competition/store/s101to120/YangHui.java
@@ -0,0 +1,31 @@
+package competition.store.s101to120;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class YangHui {
+	public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+
+		for (int i = 0; i < numRows; i++) {
+			List<Integer> newList = new ArrayList<Integer>(i + 1);
+
+			for (int j = 0; j < i + 1; j++) {
+				if (j == 0 || j == i) {
+					newList.add(j, 1);
+				} else {
+					newList.add(j, list.get(i - 1).get(j - 1) + list.get(i - 1).get(j));
+				}
+			}
+
+			list.add(newList);
+		}
+
+		return list;
+	}
+
+	public static void main(String[] args) {
+		YangHui yangHui = new YangHui();
+		yangHui.generate(8);
+	}
+}
