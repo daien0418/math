901de115d58d007d463bf692556acd440604b18b
daien <daien@bonc.com.cn>
2018-11-10 18:17:14 +0800

    修改代码，2018年11月10日18:17:13

--- src/juc/lock/Relocktest.java
+++ src/juc/lock/Relocktest.java
@@ -1,33 +1,50 @@
 package juc.lock;
+
+import java.util.Deque;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.LockSupport;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.StampedLock;
 
 public class Relocktest {
 
-	private ReentrantLock lock =  new ReentrantLock();
+	private ReentrantLock lock = new ReentrantLock();
 	private Condition condition = lock.newCondition();
 	private ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
 
-	public void service(Object obj){
+//	ThreadPoolExecutor
+//	ConcurrentLinkedDeque<E>
+//	private LinkedBlockingDeque<E>
+//	private PriorityBlockingQueue<E>
+//	private DelayQueue
+
+	public void service(Object obj) {
 		synchronized (obj) {
-			System.out.println(Thread.currentThread().getName()+"----start:"+System.currentTimeMillis());
+			System.out.println(Thread.currentThread().getName() + "----start:" + System.currentTimeMillis());
 			try {
 				obj.wait(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			System.out.println(Thread.currentThread().getName()+"-----end:"+System.currentTimeMillis());
+			System.out.println(Thread.currentThread().getName() + "-----end:" + System.currentTimeMillis());
 		}
 	}
 
-	public void notifyservice(Object obj){
+	public void notifyservice(Object obj) {
 		synchronized (obj) {
 			obj.notify();
 		}
 	}
 
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		Relocktest relocktest = new Relocktest();
 		Object object = new Object();
 		Runnable runa = new Runnable() {
