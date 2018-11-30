3e68f1c6312ace0b9485cd18dae773389f89b6e0
daien <daien@bonc.com.cn>
2018-11-30 17:46:34 +0800

    树相关题目若干，2018年11月30日17:46:32

---
+++ src/competition/store/s601to700/s681to700/SearchBST.java
@@ -0,0 +1,22 @@
+package competition.store.s601to700.s681to700;
+
+import competition.structure.TreeNode;
+
+public class SearchBST {
+
+	public TreeNode searchBST(TreeNode root, int val) {
+
+		while (root != null) {
+			if (root.val == val) {
+				return root;
+			} else if (root.val > val) {
+				root = root.left;
+			} else {
+				root = root.right;
+			}
+		}
+
+		return null;
+	}
+
+}
---
+++ src/competition/store/s801to900/s801to820/PruneTree.java
@@ -0,0 +1,35 @@
+package competition.store.s801to900.s801to820;
+
+import competition.structure.TreeNode;
+
+public class PruneTree {
+
+	public TreeNode pruneTree(TreeNode root) {
+		if (isZero(root))
+			return null;
+		if (isZero(root.left)) {
+			root.left = null;
+		} else {
+			pruneTree(root.left);
+		}
+		if (isZero(root.right)) {
+			root.right = null;
+		} else {
+			pruneTree(root.right);
+		}
+		return root;
+	}
+
+	private boolean isZero(TreeNode root) {
+		if (root == null)
+			return true;
+		if (root.val == 1)
+			return false;
+		return isZero(root.left) && isZero(root.right);
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
